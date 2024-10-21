17eb97a1e6c12af72c80498d8532346a0031cd8d
sasmith2050 <sasmith2050@gmail.com>
2024-10-20 18:43:28 -0600

    added JavaScript and for loop

---
+++ assets/scripts.js
@@ -0,0 +1,19 @@
+const topic = ["HTML", "CSS", "Git", "JavaScript"];
+/*
+
+if (topic === 'HTML') {
+    console.log("Let's Study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's Study CSS!");
+} else if (topic === 'Git') {
+    console.log("Let's Study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's Study JavaScript");
+} else {
+    console.log('Please try again!');
+}""
+*/
+
+for(let x = 0; x < topic.length; x++) {
+    console.log(topic[x]);
+}
